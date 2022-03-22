0bb18128d1daebacb0aa20cc3ed849c42410a2ee
Noelia Ibañez <alu0101225555@ull.edu.es>
2022-03-22 15:07:16 +0000

    Estructura p6

--- src/prueba.ts
+++
@@ -1,6 +0,0 @@
-/** Prueba */
-var mensaje:string = "Hola Mundo";
-console.log(mensaje);
-
-// let a = new classname(paso parámetros)
-// expect función: a.nombre_funcion().to.be.equal("")
--- src/prueba/persona.ts
+++
@@ -1,6 +0,0 @@
-export class Persona {
-    constructor (nombre: string, apellidos: [string, string], edad: number) {
-    }
-}
-
-//Para que los atributos los hereden otra clase -> deben ser públicos
